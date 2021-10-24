a051c12988a12eb2f458ecd7ff6f3e335bcd0c45
Seyed Sahil <seyedsah@gmail.com>
2021-10-24 22:46:08 +0530

    Initial commit

---
+++ src/main/java/org/sydlabz/demo/springcloud/eureka/EurekaServerDemoApplication.java
@@ -0,0 +1,15 @@
+package org.sydlabz.demo.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sydlabz/demo/springcloud/eureka/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sydlabz.demo.springcloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
